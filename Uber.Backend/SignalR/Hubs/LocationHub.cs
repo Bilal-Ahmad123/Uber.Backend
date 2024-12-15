55e704c43631d295913686f424284d3f4a54c363
Bilal Ahmad <ba269953@gmail.com>
2024-12-15 17:58:16 +0500

    Server Changes for signalr location handling

--- Uber.Backend/SignalR/Hubs/LocationHub.cs
+++ Uber.Backend/SignalR/Hubs/LocationHub.cs
@@ -1,10 +1,11 @@
 ﻿
 using Microsoft.AspNetCore.SignalR;
+using Uber.Application.Models;
 
 namespace Uber.Backend.Presentation.SignalR.Hubs;
 public sealed class LocationHub:Hub
 {
-    public async Task SendMessage(string location)
+    public async Task SendMessage(Location location)
     {
         await Clients.All.SendAsync("Send",location);
     }
