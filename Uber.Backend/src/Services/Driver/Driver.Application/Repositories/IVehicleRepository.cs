8ba1982a892c631b2dcc1e8b048b777a2a9fcbf4
Bilal Ahmad <ba269953@gmail.com>
2025-03-17 19:40:08 +0500

    added api for getting vehicle details

---
+++ Uber.Backend/src/Services/Driver/Driver.Application/Repositories/IVehicleRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleModel = Driver.Domain.Models.Vehicle.Vehicle;
+
+namespace Driver.Application.Repositories;
+public interface IVehicleRepository
+{
+    Task<VehicleModel> GetVehicleDetails(Guid driverId);
+}
